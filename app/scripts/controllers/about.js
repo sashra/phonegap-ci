7a35f0f14420edb16523a35fc14e5bd11e2ed76b
sashra <shivani.ashra92@gmail.com>
2015-03-09 16:54:09 +0530

    first commit

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name phonegapCiApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the phonegapCiApp
+ */
+angular.module('phonegapCiApp')
+  .controller('AboutCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name phonegapCiApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the phonegapCiApp
+ */
+angular.module('phonegapCiApp')
+  .controller('MainCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
